4eee0c69b24a0279b104cd29911330c2a95f40d6
Deiaa-alzyat <yousef.alzyat11@gmail.com>
2024-05-18 23:10:34 +0000

    0x02. Redis basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,71 +1,22 @@
 #!/usr/bin/env python3
-""" Uses the requests module to obtain the HTML content of
-    a particular URL and returns it
-"""
-import requests
+""" Implementing an expiring web cache and tracker
+    obtain the HTML content of a particular URL and returns it """
 import redis
-from functools import wraps
-from typing import Callable
-import time
-
+import requests
 r = redis.Redis()
-r.flushdb()
+count = 0
 
 
-def cache_count(method: Callable) -> Callable:
-    """ Cache Count Decorator """
-    @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper Function """
-        count_key = f"count:{url}"
-        cached_url = f"cached:{url}"
-        cached = r.get(cached_url)
-
-        if cached:
-            r.incr(count_key)
-            return (cached.decode('utf-8'))
-
-        content = method(url)
-        r.setex(cached_url, 10, content)
-        r.set(count_key, 1)
-        return (content)
-
-    return (wrapper)
-
-
-@cache_count
 def get_page(url: str) -> str:
-    """ Get Page Function """
-    try:
-        res = requests.get(url).text
-        return (res)
-    except requests.RequestException as e:
-        return ("")
+    """ track how many times a particular URL was accessed in the key
+        "count:{url}"
+        and cache the result with an expiration time of 10 seconds """
+    r.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    r.incr(f"count:{url}")
+    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-
-    time.sleep(5)
-    print("After 5")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-    time.sleep(4)
-    print("After 9")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-    time.sleep(1)
-    print("After 10")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
+    get_page('http://slowwly.robertomurray.co.uk')
